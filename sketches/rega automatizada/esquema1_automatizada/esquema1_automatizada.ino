a0171a9ad13ffcf5e4d4604504498bd28415971a
smjacques <saulojacques@protonmail.com>
2017-10-13 23:59:27 -0300

    pictures, sketches and slides updated

--- sketches/rega automatizada/esquema1_automatizada/esquema1_automatizada.ino
+++ sketches/rega automatizada/esquema1_automatizada/esquema1_automatizada.ino
@@ -1,5 +1,5 @@
 /*sketch simples, mostrando apenas o output com resultados da umidade
-Bom notar as infos de "umidade" (%) e "val_umid" (valor bruto)
+Bom notar as infos de "umidade" (%) e "umidade_volt" (valor bruto)
 */
 
 const int val_umid = 0; // Analog pin 0
@@ -9,13 +9,15 @@ void setup() {
 }
      
 void loop() {
-  int umidade = analogRead(val_umid);    
-  umidade = constrain(umidade,400,1023);
+  int umidade_volt = analogRead(val_umid);
+  int umidade = umidade_volt;
+  umidade = constrain(umidade_volt,400,1023);
   umidade = map(umidade,400,1023,100,0);
 
   Serial.println("Umidade (valor bruto)");
-  Serial.println(val_umid);
+  Serial.println(umidade_volt);
   Serial.println("Umidade (%)");
   Serial.println(umidade);
+  delay(2000);
   
 }
